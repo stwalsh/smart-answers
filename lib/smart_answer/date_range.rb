443c08383fd02ca4d28e15c406593644fd03b1e2
James Mead <james@floehopper.org>
2015-08-03 10:21:52 +0100

    Introduce new DateRange class
    This is a value object [1] representing a range of dates. It is instantiated
    using a `begins_on` date and an `ends_on` date. If `begins_on` is not specified
    then the range starts from a negatively infinite date, and if `ends_on` is not
    specified then the range ends on a positively infinite date. The start and end
    dates are converted to a date (in case they aren't already).
    
    Since it's a value object, I've implemented equality-related methods so that
    `DateRange`'s with the same start and end dates are equal/equivalent to each
    other. These implementations are based on those in the `values` gem [2].
    
    I'm planning to use this in the new calculate-your-part-year-profit flow, but I
    expect it could also be used in some of the existing flows.
    
    [1]: http://c2.com/cgi/wiki?ValueObject
    [2]: https://rubygems.org/gems/values

---
+++ lib/smart_answer/date_range.rb
@@ -0,0 +1,47 @@
+require 'date'
+
+class Date
+  def infinite?
+    false
+  end
+end
+
+class Date::Infinity
+  def to_date
+    self
+  end
+end
+
+module SmartAnswer
+  class DateRange
+    EARLIEST_DATE = Date::Infinity.new(-1)
+    LATEST_DATE = Date::Infinity.new(+1)
+
+    attr_reader :begins_on, :ends_on
+
+    def initialize(begins_on: EARLIEST_DATE, ends_on: LATEST_DATE)
+      @begins_on = begins_on.to_date
+      @ends_on = ends_on.to_date
+    end
+
+    def include?(date)
+      (date >= @begins_on) && (date <= @ends_on)
+    end
+
+    def number_of_days
+      [@begins_on, @ends_on].any?(&:infinite?) ? Float::INFINITY : (@ends_on - @begins_on).to_i + 1
+    end
+
+    def ==(other)
+      eql?(other)
+    end
+
+    def eql?(other)
+      (self.class == other.class) && ([begins_on, ends_on] == [other.begins_on, other.ends_on])
+    end
+
+    def hash
+      self.class.hash ^ [begins_on, ends_on].hash
+    end
+  end
+end
